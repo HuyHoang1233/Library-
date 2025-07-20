3b1504a77dcfc68685e3ce593021753815790e79
LeeNhatt <thanhdanh6116@gmail.com>
2025-07-20 15:10:17 +0700

    Update Reader.java

--- src/model/Reader.java
+++ src/model/Reader.java
@@ -1,96 +1,54 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
+
 import java.io.Serializable;
 
-/**
- *
- * @author Huy Hoàng
- */
-public class Reader implements Serializable {
-	private String idReader;
-	private String nameReader;
-	private String type;
-	private String sex;
-	private String password;
-	public String getIdReader() {
-		return idReader;
-	}
-	public void setIdReader(String idReader) {
-		this.idReader = idReader;
-	}
-	public String getNameReader() {
-		return nameReader;
-	}
-	public void setNameReader(String nameReader) {
-		this.nameReader = nameReader;
-	}
-	public String getType() {
-		return type;
-	}
-	public void setType(String type) {
-		this.type = type;
-	}
-	public String getSex() {
-		return sex;
-	}
-	public void setSex(String sex) {
-		this.sex = sex;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
+public class Reader extends User implements Serializable {
+    private String idReader;
+    private String type;
+    private String sex;
 
+    public Reader(String username, String password, String idReader, String type, String sex) {
+        super(username, password); // gọi constructor từ lớp cha User
+        this.idReader = idReader;
+        this.type = type;
+        this.sex = sex;
+    }
 
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((idReader == null) ? 0 : idReader.hashCode());
-		result = prime * result + ((nameReader == null) ? 0 : nameReader.hashCode());
-		result = prime * result + ((sex == null) ? 0 : sex.hashCode());
-		result = prime * result + ((type == null) ? 0 : type.hashCode());
-		return result;
-	}
+    public String getIdReader() {
+        return idReader;
+    }
 
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Reader other = (Reader) obj;
-		if (idReader == null) {
-			if (other.idReader != null)
-				return false;
-		} else if (!idReader.equals(other.idReader))
-			return false;
-		if (nameReader == null) {
-			if (other.nameReader != null)
-				return false;
-		} else if (!nameReader.equals(other.nameReader))
-			return false;
-		if (sex == null) {
-			if (other.sex != null)
-				return false;
-		} else if (!sex.equals(other.sex))
-			return false;
-		if (type == null) {
-			if (other.type != null)
-				return false;
-		} else if (!type.equals(other.type))
-			return false;
-		return true;
-	}
+    public void setIdReader(String idReader) {
+        this.idReader = idReader;
+    }
 
-	public String toString() {
-		return "Reader [idReader=" + idReader + ", nameReader=" + nameReader + ", type=" + type + ", sex=" + sex
-				+ ", password=" + password + "]";
-	}
+    public String getType() {
+        return type;
+    }
 
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    @Override
+    public void showMenu() {
+        System.out.println("=== Reader Menu ===");
+        System.out.println("1. View borrowed books");
+        System.out.println("2. Search books");
+        System.out.println("0. Exit");
+    }
+
+    @Override
+    public String toString() {
+        return "Reader [username=" + username + ", idReader=" + idReader +
+               ", type=" + type + ", sex=" + sex + ", password=" + password + "]";
+    }
 }
