1a5f9a8a0c4cc029d1628025e65a7d6d3aac2acd
LeeNhatt <thanhdanh6116@gmail.com>
2025-07-20 15:07:23 +0700

    Update Librarian.java

--- src/model/Librarian.java
+++ src/model/Librarian.java
@@ -1,54 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
+
 import java.io.Serializable;
-/**
- *
- * @author Huy Hoàng
- */
-public class Librarian implements Serializable {
-	private String nameUser;
-	private String password;
-	public String getNameUser() {
-		return nameUser;
-	}
-	public void setNameUser(String nameUser) {
-		this.nameUser = nameUser;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
 
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((nameUser == null) ? 0 : nameUser.hashCode());
-		return result;
-	}
+public class Librarian extends User implements Serializable {
+    public Librarian(String username, String password) {
+        super(username, password);
+    }
 
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Librarian other = (Librarian) obj;
-		if (nameUser == null) {
-			if (other.nameUser != null)
-				return false;
-		} else if (!nameUser.equals(other.nameUser))
-			return false;
-		return true;
-	}
+    @Override
+    public void showMenu() {
+        System.out.println("=== Librarian Menu ===");
+        System.out.println("1. Add new book");
+        System.out.println("2. View all books");
+        System.out.println("3. Manage readers");
+        System.out.println("0. Exit");
+    }
 
-	public String toString() {
-		return "Librarian [nameUser=" + nameUser + ", password=" + password + "]";
-	}
-
+    @Override
+    public String toString() {
+        return "Librarian [username=" + username + ", password=" + password + "]";
+    }
 }
